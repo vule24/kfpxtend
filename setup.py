5caeb536d4825106d8cd28c2cfc4b033db0f2af6
Vu Le <vult5@onemount.com>
2020-09-16 05:29:00 +0000

    update .gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="easykubeflow",
-    version="0.0.8",
+    version="0.0.9",
     author="Le Tuan Vu",
     author_email="ltnv24@gmail.com",
     description="High level of kubeflow SDK",
